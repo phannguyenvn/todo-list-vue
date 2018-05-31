5b92c912e5eed9d87a597f48d8a69851700814ee
Phan Nguyen <phannguyen@VN-LW70186-M.local>
2018-05-31 17:31:40 +0700

    todo item

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
     <img src="./assets/logo.png">
     <router-view/>
   </div>
@@ -12,12 +12,22 @@ export default {
 </script>
 
 <style>
+
+* {
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
 </style>
