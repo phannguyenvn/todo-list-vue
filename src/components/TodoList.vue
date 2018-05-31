7cd8f569f2fef7b11fdd1eaf4c5bcbee4d76a811
LW70186 <phannguyen@VN-LW70186-M.local>
2018-05-31 09:45:42 +0700

    init source

---
+++ src/components/TodoList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input type="text" class="todo-input" placeholder="What needs to be done">
+    <h1>todo list</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo-list',
+  data () {
+    return {
+      msg: 'Welcome to Your Todo List App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+  .todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;
+    &:focus {
+      outline: 0;
+    }
+  }
+
+</style>
