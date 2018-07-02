0e82932b4038431fa4fccdd3a0720273a7c4bf83
VN_LW70186-M <phannguyen@Phans-MacBook-Pro.local>
2018-07-02 23:29:22 +0700

    Authentication

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,28 @@
 <template>
   <div id="app" class="container">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <img src="./assets/logo.png" class="logo">
+    <todo-list></todo-list>
   </div>
 </template>
 
 <script>
+import TodoList from './components/TodoList'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    TodoList,
+  }
 }
 </script>
 
 <style>
-
-* {
-  box-sizing: border-box;
-}
-
 .container {
   max-width: 600px;
   margin: 0 auto;
 }
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.logo {
+  display: block;
+  margin: 20px auto;
+  height: 75px;
 }
-
 </style>
